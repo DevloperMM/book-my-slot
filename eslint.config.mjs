319c121caebfd8acd54a7c056f9056a5ac5c76f5
DevloperMM <mangalmurtivarshney2810@gmail.com>
2026-08-03 14:13:20 +0530

    chore: update project structure, husky pre-commit hooks, and linting/formatting configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,28 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import nextPrettier from 'eslint-config-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  nextPrettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts'
   ]),
+  {
+    rules: {
+      'no-console': 'error',
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      quotes: ['error', 'single', { allowTemplateLiterals: true }]
+    }
+  }
 ]);
 
 export default eslintConfig;
